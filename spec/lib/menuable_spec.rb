1d5b7c36929dfc662785da584eb23d0451c2a30d
Rob Yurkowski <rob@yurkowski.net>
2013-02-11 15:53:50 -0500

    Add initial spec for menuable and viewable.

---
+++ spec/lib/menuable_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'afterburnercms/menuable'
+
+describe Menuable do
+  it "exists" do
+    true.should be_true
+  end
+end
---
+++ spec/lib/viewable_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'afterburnercms/viewable'
+
+describe Viewable do
+  it "exists" do
+    true.should be_true
+  end
+end
