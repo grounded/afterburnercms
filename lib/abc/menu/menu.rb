fdfe640eea74f6362e9bb829b0e11ad19511a599
Rob Yurkowski <rob@yurkowski.net>
2013-02-16 05:07:55 -0500

    Add basic menu structure (WIP).

--- lib/abc/menu/menu.rb
+++ lib/abc/menu/menu.rb
@@ -13,5 +13,44 @@ module Abc
   #       it just be able to construct itself from arguments?
   #       Are there caching benefits?
   class Menu
+    include ActionView::Helpers::TagHelper
+    include ActionView::Context # Nested content_tags.
+
+    attr_accessor :nodes, :menu_element
+    attr_reader :list_element_pair
+    protected :nodes=
+
+    def initialize(els = [])
+      self.nodes = els
+      self.menu_element = :nav
+      self.list_element_pair = [:ul, :li]
+    end
+
+    # Outputs the menu in HTML format.
+    #
+    # @return An HTML-safe string containing the markup of the menu in question.
+    def to_html
+      content_tag(menu_element) do
+        content_tag(list_element_pair.first) do
+          # Reduce will concat into a first SafeBuffer here, so if the first
+          # node is html_safe, this will produce expected outcome. And of course,
+          # MenuEntries are expected to be html_safe.
+          # TODO: Should they be? I'm not sure how we'd render without that.
+          #       I think filtering would have to be at a finer level.
+          nodes.map {|n| n.to_html(list_element_pair.last) }.reduce(:<<) if nodes.count.nonzero?
+        end
+      end.html_safe
+    end
+
+    # Set the elements that are used to determine what contains a series of
+    # nodes and the actual nodes itself. Defaults to [:ul, :li].
+    #
+    # @param pair [Array] A pair of HTML element names
+    def list_element_pair=(pair)
+      raise "List element pair must be an array" unless pair.is_a?(Array)
+      raise "List element pair must specify two element types" unless pair.count == 2
+      raise "List element pair should be symbols" unless pair.all? {|e| e.is_a?(Symbol)}
+      @list_element_pair = pair
+    end
   end
 end
--- spec/lib/menu/menu_spec.rb
+++ spec/lib/menu/menu_spec.rb
@@ -1,10 +1,61 @@
 require 'spec_helper'
 require 'abc/menu/menu'
 
+class MockMenuEntry
+  include ActionView::Helpers::TagHelper
+  def initialize(name); @name = name; end
+  def to_html(list_el = :li); content_tag(list_el, @name).html_safe; end
+end
+
 module Abc
   describe Menu do
-    it "exists" do
-      true.should be_true
+    let(:entry1) { MockMenuEntry.new("node 1") }
+    let(:entry2) { MockMenuEntry.new("node 2") }
+    let(:menu) { Menu.new([entry1, entry2]) }
+
+    describe "listing" do
+
+      it "lists nothing when empty" do
+        subject.nodes.should be_empty
+      end
+
+      it "lists root nodes" do
+        menu.nodes.should == [entry1, entry2]
+      end
     end
+
+    describe "rendering" do
+      describe "HTML" do
+        it "asks its nodes to render themselves" do
+          entry1.should_receive(:to_html).and_return("<li>node 1</li>".html_safe)
+          menu.to_html
+        end
+
+        it "draws its nodes in order" do
+          menu.to_html.should == "<nav><ul>%s%s</ul></nav>" % [entry1.to_html, entry2.to_html]
+        end
+
+        it "is considered HTML safe" do
+          menu.to_html.html_safe?.should be_true
+        end
+
+        it "allows an alternate root element" do
+          menu.menu_element = :div 
+          menu.to_html.should match(/\A<div/)
+        end
+
+        it "allows an alternate list element pair" do
+          menu.list_element_pair = [:menu, :div]
+          menu.to_html.should match(/\A<nav><menu><div>/)
+        end
+
+        # TODO: Add specs for list element pair squawks, or refactor these into
+        #       two separate param. How do we handle codependence, then?
+        #       i.e. since this is a ul and an li, what if people change
+        #       the ul but not the li? Is this something that our API can make
+        #       clearer than it is at the moment?
+      end
+    end
+
   end
 end
