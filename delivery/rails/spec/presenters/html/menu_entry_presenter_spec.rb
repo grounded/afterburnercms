af980c8c85023a443e74f0157a820521537dd830
Philip Arndt <parndt@gmail.com>
2013-03-06 17:14:35 +1300

    Allow Rails autoloading to work by properly namespacing presenter components.

--- delivery/rails/spec/presenters/html/menu_entry_presenter_spec.rb
+++ delivery/rails/spec/presenters/html/menu_entry_presenter_spec.rb
@@ -8,42 +8,40 @@ class MockMenuEntry
 end
 
 module Abc
-  module Presenters
-    module Html
-      describe MenuEntryPresenter do
-        let(:menu_item) { MockMenuEntry.new(:title => "Root") }
-        let(:child) { MockMenuEntry.new(:title => "Child") }
-        subject { MenuEntryPresenter.new(menu_item) }
+  module Html
+    describe MenuEntryPresenter do
+      let(:menu_item) { MockMenuEntry.new(:title => "Root") }
+      let(:child) { MockMenuEntry.new(:title => "Child") }
+      subject { MenuEntryPresenter.new(menu_item) }
 
-        it "renders itself" do
-          subject.to_html.should == "<li>Root</li>"
+      it "renders itself" do
+        subject.to_html.should == "<li>Root</li>"
+      end
+
+      it "renders with the list enumerator of list_element_pair" do
+        subject.list_element_pair = [:nav, :div]
+        subject.to_html.should == "<div>Root</div>"
+      end
+
+      it "raises an error if its content is not marked html_safe" do
+        subject.stub!(:render_as_html).and_return("unsafe string")
+        expect { subject.to_html }.to raise_error(::ActiveSupport::SafeBuffer::SafeConcatError)
+      end
+
+      context "with children" do
+        before { menu_item.stub!(:children).and_return([child]) }
+        # TODO: Remove this diagnostic spec.
+        it "child count should be one" do
+          subject.menu_entry.children.count.should == 1
         end
 
-        it "renders with the list enumerator of list_element_pair" do
+        it "renders its children" do
+          subject.to_html.should == "<li>Root<ul><li>Child</li></ul></li>"
+        end
+
+        it "wraps children in the passed list element" do
           subject.list_element_pair = [:nav, :div]
-          subject.to_html.should == "<div>Root</div>"
-        end
-
-        it "raises an error if its content is not marked html_safe" do
-          subject.stub!(:render_as_html).and_return("unsafe string")
-          expect { subject.to_html }.to raise_error(::ActiveSupport::SafeBuffer::SafeConcatError)
-        end
-
-        context "with children" do
-          before { menu_item.stub!(:children).and_return([child]) }
-          # TODO: Remove this diagnostic spec.
-          it "child count should be one" do
-            subject.menu_entry.children.count.should == 1
-          end
-
-          it "renders its children" do
-            subject.to_html.should == "<li>Root<ul><li>Child</li></ul></li>"
-          end
-
-          it "wraps children in the passed list element" do
-            subject.list_element_pair = [:nav, :div]
-            subject.to_html.should == "<div>Root<nav><div>Child</div></nav></div>"
-          end
+          subject.to_html.should == "<div>Root<nav><div>Child</div></nav></div>"
         end
       end
     end
--- delivery/rails/spec/presenters/html/menu_presenter_spec.rb
+++ delivery/rails/spec/presenters/html/menu_presenter_spec.rb
@@ -11,44 +11,42 @@ class MockMenu
   attr_accessor :children
   def initialize(opts = {}); @children = opts[:children] || []; end
 end
-  
+
 
 module Abc
-  module Presenters
-    module Html
-      describe MenuPresenter do
-        let(:entry1) { MockMenuEntry.new("node 1") }
-        let(:entry2) { MockMenuEntry.new("node 2") }
-        let(:menu) { MockMenu.new(:children => [entry1, entry2]) }
-        let(:presenter) { MenuPresenter.new(menu) }
-        # TODO: Add specs for list element pair squawks, or refactor these into
-        #       two separate param. How do we handle codependence, then?
-        #       i.e. since this is a ul and an li, what if people change
-        #       the ul but not the li? Is this something that our API can make
-        #       clearer than it is at the moment?
+  module Html
+    describe MenuPresenter do
+      let(:entry1) { MockMenuEntry.new("node 1") }
+      let(:entry2) { MockMenuEntry.new("node 2") }
+      let(:menu) { MockMenu.new(:children => [entry1, entry2]) }
+      let(:presenter) { MenuPresenter.new(menu) }
+      # TODO: Add specs for list element pair squawks, or refactor these into
+      #       two separate param. How do we handle codependence, then?
+      #       i.e. since this is a ul and an li, what if people change
+      #       the ul but not the li? Is this something that our API can make
+      #       clearer than it is at the moment?
 
-        it "asks its children to render themselves" do
-          entry1.should_receive(:to_html).and_return("<li>node 1</li>".html_safe)
-          presenter.to_html
-        end
+      it "asks its children to render themselves" do
+        entry1.should_receive(:to_html).and_return("<li>node 1</li>".html_safe)
+        presenter.to_html
+      end
 
-        it "draws its children in order" do
-          presenter.to_html.should == "<nav><ul>%s%s</ul></nav>" % [entry1.to_html, entry2.to_html]
-        end
+      it "draws its children in order" do
+        presenter.to_html.should == "<nav><ul>%s%s</ul></nav>" % [entry1.to_html, entry2.to_html]
+      end
 
-        it "is considered HTML safe" do
-          presenter.to_html.html_safe?.should be_true
-        end
+      it "is considered HTML safe" do
+        presenter.to_html.html_safe?.should be_true
+      end
 
-        it "allows an alternate root element" do
-          presenter.menu_element = :div
-          presenter.to_html.should match(/\A<div/)
-        end
+      it "allows an alternate root element" do
+        presenter.menu_element = :div
+        presenter.to_html.should match(/\A<div/)
+      end
 
-        it "allows an alternate list element pair" do
-          presenter.list_element_pair = [:menu, :div]
-          presenter.to_html.should match(/\A<nav><menu><div>/)
-        end
+      it "allows an alternate list element pair" do
+        presenter.list_element_pair = [:menu, :div]
+        presenter.to_html.should match(/\A<nav><menu><div>/)
       end
     end
   end
