f5b768cec7f837000695aff1062bf0d2013bd6ee
Rob Yurkowski <rob@yurkowski.net>
2013-03-08 13:50:35 -0500

    Use be_kind_of in class comparison.

--- core/spec/entities/menu/menu_entry_spec.rb
+++ core/spec/entities/menu/menu_entry_spec.rb
@@ -5,7 +5,7 @@ module Abc
   describe MenuEntry do
     it "wraps passed children" do
       e = MenuEntry.new('My title', [{:title => 'Child 1'}])
-      e.children.first.class.should == MenuEntry
+      expect(e.children.first).to be_kind_of Abc::MenuEntry
     end
   end
 end
