825f8df66fa37508d6154a0fd5ff691f71b63569
Rob Yurkowski <rob@yurkowski.net>
2013-03-08 13:45:18 -0500

    Expand test coverage for menu_entry.

--- core/spec/entities/menu/menu_entry_spec.rb
+++ core/spec/entities/menu/menu_entry_spec.rb
@@ -3,5 +3,9 @@ require 'menu/menu_entry'
 
 module Abc
   describe MenuEntry do
+    it "wraps passed children" do
+      e = MenuEntry.new('My title', [{:title => 'Child 1'}])
+      e.children.first.class.should == MenuEntry
+    end
   end
 end
