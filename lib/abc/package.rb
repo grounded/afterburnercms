df8a9afd6edcf743dd419b3eb8d5c37459f6f316
Rob Yurkowski <rob@yurkowski.net>
2013-03-16 03:05:01 -0400

    Change authors and gem description.

--- lib/abc/package.rb
+++ lib/abc/package.rb
@@ -1,10 +1,10 @@
 module Abc
   RAILS_VERSION = '>= 4.0.0.beta'
   VERSION       = '0.0.1'
-  AUTHORS       = ["Rob Yurkowski"]
-  EMAILS        = ["rob@yurkowski.net"]
+  AUTHORS       = ["The Afterburner CMS Team"]
+  EMAILS        = ["info@afterburnercms.com"]
   HOMEPAGE      = "http://www.afterburnercms.com/"
-  SUMMARY       = "An abecedarium."
+  SUMMARY       = "An easy-to-use, easy-to-extend CMS."
   DESCRIPTION   = SUMMARY
 
 
